2445a137ac97aebb1816799f535539fe365acf4c
filipe-gomes <filipegomes404@gmail.com>
2020-09-29 16:37:03 -0500

    Halfway through migrating state management to graphQL from redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,10 +4,10 @@ import 'firebase/auth';
 
 const config = {
   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
+  authDomain: 'astro-db.firebaseapp.com',
+  databaseURL: 'https://astro-db.firebaseio.com',
+  projectId: 'astro-db',
+  storageBucket: 'astro-db.appspot.com',
   messagingSenderId: '850995411664',
   appId: '1:850995411664:web:7ddc01d597846f65'
 };
